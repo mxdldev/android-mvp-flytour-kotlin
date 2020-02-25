5beffe79595798e17f43255c1a6534ec82d3e068
geduo83 <geduo_83@163.com>
2020-02-25 17:53:31 +0800

    code perfect

--- module_test/src/main/java/com/fly/tour/test/MainActivity.kt
+++ module_test/src/main/java/com/fly/tour/test/MainActivity.kt
@@ -2,6 +2,8 @@ package com.fly.tour.test
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import com.fly.tour.common.util.ToastUtil
 import com.fly.tour.test.view.MyView2
 import com.fly.tour.test.view.Person1
 import kotlinx.android.synthetic.main.activity_main.*
@@ -11,9 +13,16 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        btn.setOnClickListener {
-            //view.test()
 
-        }
+        btn.setOnClickListener{ v -> ToastUtil.showToast("ok") }
+        btn.setOnClickListener(){ v -> ToastUtil.showToast("ok") }
+        btn.setOnClickListener({ v -> ToastUtil.showToast("ok") })
+        btn.setOnClickListener { }
+        btn.setOnClickListener(object :View.OnClickListener{
+            override fun onClick(v: View?) {
+                TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+            }
+
+        })
     }
 }
